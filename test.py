4c90afc4e794cffec2f3421250fe6037c71f28e8
kotmakrus <sg.glazkov@gmail.com>
2024-08-02 20:33:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-f = open('27_A.txt')
-n = int(f.readline())
-k, s = 89, 0
-max_sum = 0
-min_len = 10 ** 10
+f = open('27_B.txt')
+n = int(f.readline())   # количество чисел
+k, total = 89, 0    # изначальные данные
+max_sum, min_len = 0, 10 ** 10    #макс. сумма и ее мин. длина
 
-elements = {0: (0, 0)}
-res = []
 
-for j in range(1, n + 1):
-    s += int(f.readline())
-    if s % k in elements:
-        if s - elements[s % k][0] > max_sum:
-            max_sum = s - elements[s % k][0]
-            min_len = 10 ** 10
-        if s - elements[s % k][0] == max_sum:
-            min_len = min(min_len, j - elements[s % k][1])
-    else:
-        elements[s % k] = (s, j)
+elements = {0: (0, 0)}  # словарь, ключ - текущая сумма, значение - ее длина
+
+for j in range(1, n + 1):   # идем по всем числам
+    total += int(f.readline())  # вводим и сразу суммируем в текущую сумму
+    if total % k in elements:   # если сумма делится на K=89
+        if total - elements[total % k][0] > max_sum: # проверяем максимальна ли она, если да
+            max_sum = total - elements[total % k][0]    # сохраняем ее
+            min_len = 10 ** 10  # мин. длина более не актуальна, поскольку был найден новый максимум
+        if total - elements[total % k][0] == max_sum:   # если текущая сумма равна максимальной
+            min_len = min(min_len, j - elements[total % k][1])  # переписываем мин. длину
+    else:    # если сумма не делится на K=89
+        elements[total % k] = (total, j)    # добавляем ее значение и позицию элемента в словарь для дальнейших возможных
+                                            # сумм с такими же остатками
 
 print(min_len)
